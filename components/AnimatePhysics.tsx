0dd1e6461863410caf4f63afbcf775b4842eff95
victory-et <tuduov@gmail.com>
2024-04-25 10:46:44 +0100

    physics animation

---
+++ components/AnimatePhysics.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  velocityPerFrame,
+  velocityPerSecond,
+  PhysicsSpringOptions,
+  interpolate,
+  inertia,
+  attract,
+  attractExpo,
+  createAttractor,
+  cubicBezier,
+} from "popmotion";
+
+const AnimatePhysics = () => {
+  // animate a rounded ball div with the inertia
+  
+  return <div>AnimatePhysics</div>;
+};
+
+export default AnimatePhysics;
