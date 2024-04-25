be786916d0e20edec49afbdfa773b071cb3d9aa8
victory-et <tuduov@gmail.com>
2024-04-25 10:47:10 +0100

    app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import AnimateCircular from "@/components/AnimateCircular";
+import CollisionAnimation from "@/components/AnimateCollision";
+import AnimateGravity from "@/components/AnimateGravity";
+import SvgMorph from "@/components/AnimateMorph";
 import AnimateProps from "@/components/AnimateProps";
+import AnimateSpring from "@/components/AnimateSpring";
 import BallAnimate from "@/components/BallAnimate";
 import Image from "next/image";
 
@@ -6,7 +11,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <AnimateProps/> */}
-      <BallAnimate />
+      {/* <BallAnimate /> */}
+      {/* <AnimateGravity/> */}
+      {/* <CollisionAnimation/> */}
+      {/* <AnimateSpring/> */}
+      <SvgMorph/>
+      {/* <AnimateCircular/> */}
     </main>
   );
 }
